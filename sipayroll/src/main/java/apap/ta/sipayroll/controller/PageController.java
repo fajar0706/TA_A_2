def5edc2585026c8f22560060ef370563653d12c
Akhmad Diponegoro Widodo <adiponew@gmail.com>
2020-11-28 19:27:36 +0700

    feat: add page controller

---
+++ sipayroll/src/main/java/apap/ta/sipayroll/controller/PageController.java
@@ -0,0 +1,21 @@
+package apap.ta.sipayroll.controller;
+import apap.tutorial.traveloke.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/")
+    public String home(Model model)
+    {
+        return "home";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
