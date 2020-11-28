0f5727f6e38cc95512602633e2f6bda9e5e2ecf8
Akhmad Diponegoro Widodo <adiponew@gmail.com>
2020-11-28 19:21:25 +0700

    fix: add annotation

--- sipayroll/src/main/java/apap/ta/sipayroll/repository/GajiDb.java
+++ sipayroll/src/main/java/apap/ta/sipayroll/repository/GajiDb.java
@@ -2,7 +2,9 @@ package apap.ta.sipayroll.repository;
 
 import apap.ta.sipayroll.model.GajiModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GajiDb extends JpaRepository<GajiModel,Long> {
 
 }
