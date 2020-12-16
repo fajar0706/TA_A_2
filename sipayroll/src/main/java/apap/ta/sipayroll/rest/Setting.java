34b38a07d7e0fce4921e19fd854b5e9dcdac5a05
Akhmad Diponegoro Widodo <45806254+akhmadiponegoro@users.noreply.github.com>
2020-12-17 02:57:36 +0700

    feat: webservice 17 & 18 (#35)
    * chore: update dependencies
    
    * chore: add security package
    
    * chore: add login.html
    
    * fix: add annotation
    
    * feat: add page controller
    
    * chore: add home.html
    
    * feat: add logout feature
    
    * feat: add authorization
    
    * feat: add user configuration
    
    * fix: refactor userdetailsserviceimpl
    
    * chore: update user interfaces
    
    * feat: add authorization
    
    * feat: add more authorization
    
    * feat: webservice

---
+++ sipayroll/src/main/java/apap/ta/sipayroll/rest/Setting.java
@@ -0,0 +1,5 @@
+package apap.ta.sipayroll.rest;
+
+public class Setting {
+    final public static String siPegawaiUrl = "https://si-pegawai.herokuapp.com/api/v1/pegawai";
+}
---
+++ sipayroll/src/main/java/apap/ta/sipayroll/rest/UserDetail.java
@@ -0,0 +1,87 @@
+package apap.ta.sipayroll.rest;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Date;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserDetail {
+    @JsonProperty("username")
+    private String username;
+
+    @JsonProperty("nama")
+    private String nama;
+
+    @JsonProperty("noTelepon")
+    private String noTelepon;
+
+    @JsonProperty("tempatLahir")
+    private String tempatLahir;
+
+    @JsonProperty("tanggalLahir")
+    private Date tanggalLahir;
+
+    @JsonProperty("alamat")
+    private String alamat;
+
+    @JsonProperty("roleId")
+    private Long roleId;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNoTelepon() {
+        return noTelepon;
+    }
+
+    public void setNoTelepon(String noTelepon) {
+        this.noTelepon = noTelepon;
+    }
+
+    public String getTempatLahir() {
+        return tempatLahir;
+    }
+
+    public void setTempatLahir(String tempatLahir) {
+        this.tempatLahir = tempatLahir;
+    }
+
+    public Date getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(Date tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+}
